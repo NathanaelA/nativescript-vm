7677951b570d8c82f569fa12e7fc2a6874f0973a
Nathanael Anderson <nathan@master-technology.com>
2015-05-25 22:44:36 -0500

    Initial Version of Vagrant Building system.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # nativescript-vm
-NativeScript Virtual Machine
+##NativeScript Virtual Machine  
+
+The initialize.sh Script can be ran on a fresh Ubuntu 64bit 14.04 Installation.  (I've tested it in both VMWare and VirtualBox)   
+
+This sets up a fully function VM with all the NativeScript components; including a clone of all 4 major NativeScript repo's so that you can hack on any part of the NativeScript system.
+
+If you use Vagrant and VirtualBox or just plain VirtualBox and also use a Android VM that runs on VirtualBox (Like Geny or Andy); then this VM can EASILY connect to the Android VM. 
+
+To Connect; you need the ip address of the Android VM. (Geny typically defaults to 192.168.56.100 if it is the first VM running on VirtualBox) So then you would type:
+```adb connect 192.168.56.100``` 
+
+Please note if you are using Geny you can type:
+```genyshell -c "devices list"``` 
+to get a list of devices and their status and ip address
+
+Then when you want to compile, deploy & test to your Android vm you would type:
+```tns run android --device 1```
+
+  
+Nathanael Anderson
+[http://Master-Technology.com](http://master-technology.com)
---
+++ initialize.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+
+ANDROID_SDK_FILENAME=android-sdk_r24.2-linux.tgz
+ANDROID_NDK_FILENAME=android-ndk-r10e
+
+install_pkgs() {
+  # Update & Add i386 Components that are needed
+  sudo dpkg --add-architecture i386
+  sudo apt-get update
+  sudo apt-get upgrade
+  sudo apt-get install -y libncurses5:i386 libstdc++6:i386 zlib1g:i386 lib32z1 lib32ncurses5 lib32bz2-1.0 libstdc++6:i386
+
+  # Pull in all the Development stuff
+  sudo apt-get install -y mc linux-headers-generic build-essential g++ ant git openjdk-7-jdk p7zip p7zip-full
+}
+
+install_node() {
+  echo Downloading Latest version of Node
+  sudo wget -q --output-document=/usr/src/node-latest.tar.gz http://nodejs.org/dist/node-latest.tar.gz
+  pushd /usr/src
+  echo Extracting Node...
+  sudo tar zxvf node-latest.tar.gz > /dev/null
+  sudo chown -R $USER:$USER node-v*
+  cd node-v*
+  echo Compiling Node -- This might take a while...
+  ./configure
+  make
+  sudo make install
+  popd
+}
+
+
+install_android_sdk() {
+  echo Downloading Latest version of Android SDK, this can take a while...
+  sudo wget -q --output-document=/usr/src/android-sdk.tgz  http://dl.google.com/android/$ANDROID_SDK_FILENAME
+  pushd /usr/local
+  echo Extracting Android SDK...
+  sudo tar zxvf /usr/src/android-sdk.tgz > /dev/null
+  sudo chown -R $USER:$USER /usr/local/android-sdk-linux
+  popd
+}
+
+create_exports() {
+  # export for our current session
+  export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+  export ANT_HOME=/usr/local/ant
+  export ANDROID_HOME=/usr/local/android-sdk-linux
+  export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:/usr/local/$ANDROID_NDK_FILENAME
+
+
+  # Write to the Non-Interactive bash file
+  echo 'export ANDROID_HOME=/usr/local/android-sdk-linux' >> ~/.bashrc
+  echo 'export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:/usr/local/$ANDROID_NDK_FILENAME' >> ~/.bashrc
+  echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64" >> ~/.bashrc
+  echo "export ANT_HOME=/usr/local/ant" >> ~/.bashrc
+
+  # Write to the Standard .Profile that everything uses
+  if [[ -f ~/.profile ]]; then
+    echo 'ANDROID_HOME=/usr/local/android-sdk-linux' >> ~/.profile
+    echo 'PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:/usr/local/$ANDROID_NDK_FILENAME' >> ~/.profile
+    echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64" >> ~/.profile
+    echo "export ANT_HOME=/usr/local/ant" >> ~/.profile
+  fi
+
+  if [[ -f ~/.bash_profile ]]; then
+    echo 'export ANDROID_HOME=/usr/local/android-sdk-linux' >> ~/.bash_profile
+    echo 'export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:/usr/local/$ANDROID_NDK_FILENAME' >> ~/.bash_profile
+    echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64" >> ~/.bash_profile
+    echo "export ANT_HOME=/usr/local/ant" >> ~/.bash_profile
+  fi
+}
+
+install_android_tools() {
+  echo Installing Android Components This can take a while...
+  # Yes, I fully realize I'm pulling android-17, 21 & 22.  17 & 21 are required for different parts of NativeScript; 22 is the latest
+  ( sleep 7 && while [ 1 ]; do sleep 2; echo y; done ) | android update sdk --no-ui --filter platform-tool,android-17,android-21,android-22,build-tools-22.0.1
+}
+
+install_android_ndk() {
+  echo Downloading Android NDK, this can take a while...
+  sudo wget -q --output-document=/usr/src/$ANDROID_NDK_FILENAME.7z http://dl.google.com/android/ndk/$ANDROID_NDK_FILENAME-linux-x86_64.bin
+  sudo chmod a+r /usr/src/$ANDROID_NDK_FILENAME.7z
+  sudo mkdir /usr/local/$ANDROID_NDK_FILENAME
+  sudo chmod 777 /usr/local/$ANDROID_NDK_FILENAME
+  7z x /usr/src/$ANDROID_NDK_FILENAME.7z -o/usr/local
+}
+
+install_nativescript() {
+  sudo npm install grunt -g --unsafe-perm
+  sudo npm install grunt-cli -g --unsafe-perm
+  sudo npm install nativescript -g --unsafe-perm
+}
+
+install_github_support() {
+  mkdir ~/repos
+  pushd ~/repos
+
+  git clone https://github.com/NativeScript/NativeScript
+  git clone https://github.com/NativeScript/android-runtime
+  git clone https://github.com/NativeScript/nativescript-cli
+  git clone https://github.com/NativeScript/android-metadata-generator
+
+  cd NativeScript
+  npm install
+  cd ..
+
+  cd android-runtime
+  npm install
+  cd ..
+
+  cd nativescript-cli
+  npm install
+  cd ..
+
+  cd android-metadata-generator
+  npm install
+  cd ..
+
+  popd
+}
+
+install_network_support() {
+  echo 'auto eth1' | sudo tee --append /etc/network/interfaces.d/eth1.cfg
+  echo 'iface eth1 inet dhcp' | sudo tee --append /etc/network/interfaces.d/eth1.cfg
+  sudo service networking restart
+  sleep 5
+}
+
+create_exports
+install_pkgs
+install_node
+install_android_sdk
+install_android_tools
+install_android_ndk
+install_github_support
+install_nativescript
+install_network_support
+
+echo "Done"
+
+
+
